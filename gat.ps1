3efe510b84e59e59bd745ae1c01766c60de829f0
gataken <142103246+gataken@users.noreply.github.com>
2025-12-02 12:58:39 +0900

    gat.ps1

--- gat.ps1
+++ gat.ps1
@@ -107,6 +107,7 @@ Write-Host $wishHistoryUrl -ForegroundColor Cyan
 Set-Clipboard -Value $wishHistoryUrl
 Write-Host ""
 Write-Host "チェック完了。クリップボードにURLをコピーしました。paimon.moe に貼り付けてインポートしてください。" -ForegroundColor Green
-Read-Host "Enterキーを押すと paimon.moe に移動します。"
+#Read-Host "Enterキーを押すと paimon.moe に移動します。"
 #Start-Process "https://paimon.moe/wish/import"
+Read-Host "Enterキーを押すと stardb.gg に移動します。"
 start-process "https://stardb.gg/ja/genshin/wish-import"
